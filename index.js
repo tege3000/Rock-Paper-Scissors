bd66623e4cbfefee5593f654d45234fa5398a1d5
Tito <teetoegeonu@gmail.com>
2023-04-21 13:33:43 +0100

    Add algorithm to keep scores

--- index.js
+++ index.js
@@ -48,53 +48,80 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     let result = "";
+    let playerScore = 0;
 
     if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
         result = "Its a Draw!";
+        playerScore = 0;
     }
     else if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
         result = "You Lose, Paper beats Rock";
+        playerScore = -1;
     }
     else if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
         result = "You Win, Rock beats Scissors";
+        playerScore = 1;
     }
     else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        result = "You Win, Paper beats Rock"
+        result = "You Win, Paper beats Rock";
+        playerScore = 1;
     }
     else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
         result = "Its a Draw!";
+        playerScore = 0;
     }
     else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
         result = "You Lose, Scissors beats Paper";
+        playerScore = -1;
     }
     else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
         result = "You Lose, Rock beats Scissors";
+        playerScore = -1;
     }
     else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
         result = "You Win, Scissors beats Paper";
+        playerScore = 1;
     }
     else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
         result = "Its a Draw";
+        playerScore = 0;
     }
     else {
         console.log("Player Seclection cannot be empty or anything other than rock, paper, or scissors");
     }
 
-    return result;
+    console.log(result);
+    return playerScore;
 }
 
 //game
 //
 //
 //get computer choice, player selection using prompt and call playRound 5 times
+//add algorithm to keep track of player and computer scores
+//if gameResult == 1, then player wins that round
+//if gameResult == -1 then computer wins that round
 function game() {
+    
+    let playerScore = 0;
+    let computerScore = 0;
 
     for(let i = 0; i < 5; i++) {
         let playerSelection = prompt("Choose one, Rock, Paper, or Scissors?");
         let computerSelection = getComputerChoice();
         let gameResult = playRound(playerSelection, computerSelection); 
-        console.log(`${gameResult} \n`);
+        
+
+        if(gameResult == 1) {
+            playerScore++;
+        }
+        
+        if(gameResult == -1) {
+            computerScore++;
+        } 
     }
+
+    console.log(`Player Score: ${playerScore} \nComputer Score: ${computerScore}`);
 }
 
 game();
